18b50313272a06ce760cccb7eccb0c4676b32a70
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-02 01:02:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student < User 
+class Student < User
+
+  def initialize
+    @knowelege = Array.new
+  end
+
+  def learn(new_knowelege)
+    @knowelege << new_knowelege
+  end
+
+  def knowelege
+    @knowelege
+  end 
+
 
 end
